fd56b4b89b5468b99908e329558f4214b59c0dda
Gabriel Cabral Manhães <158057306+gab-dev10@users.noreply.github.com>
2024-07-14 10:29:37 -0300

    Add files via upload

---
+++ workout_api/configs/settings.py
@@ -0,0 +1,8 @@
+from pydantic import Field
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DB_URL: str = Field(default='postgresql+asyncpg://workout:workout@localhost/workout')
+
+
+settings = Settings()
